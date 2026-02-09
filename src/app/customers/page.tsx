6abaadaed0206f46a6e950c3a5b7783c86474820
Abdur Rohim <xrratul4763@gmail.com>
2026-02-09 03:55:29 +0000

    Try fixing this error: `Runtime ReferenceError: DialogDescription is not

--- src/app/customers/page.tsx
+++ src/app/customers/page.tsx
@@ -1,4 +1,3 @@
-
 "use client"
 
 import { useState, useMemo } from "react"
@@ -44,7 +43,8 @@ import {
   DialogHeader, 
   DialogTitle, 
   DialogTrigger,
-  DialogFooter
+  DialogFooter,
+  DialogDescription
 } from "@/components/ui/dialog"
 import {
   DropdownMenu,
